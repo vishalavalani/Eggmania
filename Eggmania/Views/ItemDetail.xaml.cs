366838e49676e5a5f7d5ca7b306ab2632e8b3a4c
vishalavalani <vishal.avalani@gmail.com>
2018-03-10 19:47:46 +0530

    * Eggmania.Droid.csproj: Work on Item Detail and sub views
    * App.xaml:
    * Eggmania.csproj:
    * packages.config:
    * About.xaml:
    * ItemDetail.xaml:
    * Eggmania.iOS.csproj:
    * ItemDetail.xaml.cs:
    * PreferencesView.xaml:
    * downarrow.png:
    * downarrow.png:
    * PreferencesView.xaml.cs:
    * Resource.designer.cs:
    * downarrow.png:

--- Eggmania/Views/ItemDetail.xaml.cs
+++ Eggmania/Views/ItemDetail.xaml.cs
@@ -8,6 +8,9 @@ namespace Eggmania.Views
     public partial class ItemDetail : ContentPage
     {
         MenuItemModel menuItem;
+        PreferencesView cookingPref;
+        PreferencesView breadPref;
+
         public ItemDetail()
         {
             InitializeComponent();
@@ -17,11 +20,55 @@ namespace Eggmania.Views
         {
             InitializeComponent();
             menuItem = item;
+            stackSpecialInst.IsVisible = false;
+
+            cookingPref = new PreferencesView();
+            cookingPref.Title = "Your Cooking Preference";
+            cookingPref.SubTitle = "";
+            cookingPref.Items = new string[]{
+                "Oil",
+                "Butter",
+                "Olive Oil"
+            };
+
+            breadPref = new PreferencesView();
+            breadPref.Title = "Choose your Bread (Required)";
+            breadPref.SubTitle = "Select One";
+            breadPref.Items = new string[]{
+                "Chapati - 1 piece:",
+                "Bread - 3 piece:",
+            };
+            stackPreferences.Children.Add(cookingPref);
+            stackPreferences.Children.Add(breadPref);
         }
 
         async void DismissView(object sender, System.EventArgs e)
         {
             await Navigation.PopModalAsync(true);
+        }
+
+        void AddItemToCart(object sender, System.EventArgs e)
+        {
+
+        }
+
+        void OnSpecialInst(object sender, System.EventArgs e)
+        {
+            ToggleVisibility(stackSpecialInst, lblSpecialInst);
+        }
+
+        private void ToggleVisibility(StackLayout layout, Label lbl)
+        {
+            if (layout.IsVisible)
+            {
+                layout.IsVisible = false;
+                lbl.Text = lbl.Text.Replace("-", "+");
+            }
+            else
+            {
+                layout.IsVisible = true;
+                lbl.Text = lbl.Text.Replace("+", "-");
+            }
         }
     }
 }
---
+++ Eggmania/Views/PreferencesView.xaml.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+using static Eggmania.Views.ItemDetail;
+
+namespace Eggmania.Views
+{
+    public partial class PreferencesView : ContentView
+    {
+
+        public PreferencesView()
+        {
+            InitializeComponent();
+            stackSubPref.IsVisible = false;
+        }
+
+        public string Title
+        {
+            get
+            {
+                return lblTitle.Text;
+            }
+            set
+            {
+                lblTitle.Text = " + " + value;
+            }
+        }
+
+        public string SubTitle
+        {
+            get
+            {
+                return lblSubTitle.Text;
+            }
+            set
+            {
+                lblSubTitle.Text = value;
+            }
+        }
+
+        public string[] Items
+        {
+            set
+            {
+                pickerPref.ItemsSource = value;
+                pickerPref.SelectedIndex = 0;
+            }
+        }
+
+        public string SelectedPickerValue
+        {
+            get
+            {
+                return Convert.ToString(pickerPref.SelectedItem);
+            }
+        }
+
+
+
+        void OnTapPref(object sender, System.EventArgs e)
+        {
+            ToggleVisibility(stackSubPref, lblTitle);
+        }
+
+        private void ToggleVisibility(StackLayout layout, Label lbl)
+        {
+            if (layout.IsVisible)
+            {
+                layout.IsVisible = false;
+                lbl.Text = lbl.Text.Replace("-", "+");
+            }
+            else
+            {
+                layout.IsVisible = true;
+                lbl.Text = lbl.Text.Replace("+", "-");
+            }
+            //Action<double> callback = input => layout.HeightRequest = input;
+            //double startingHeight = 0;
+            //double endingHeight = layout.Height;
+            //Easing easing = Easing.SinIn;
+            //uint rate = 10;
+            //uint length = 200;
+            //layout.Animate("invis", callback, startingHeight, endingHeight, rate, length, easing);
+
+        }
+    }
+}
